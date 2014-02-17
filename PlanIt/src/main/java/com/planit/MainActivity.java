0b38c8087fffa0baa927d9faaa6c7fbd797ae954
StevenCurran <scurran14@qub.ac.uk>
2014-02-17 23:22:02 +0000

    add in a calendar add event

--- PlanIt/src/main/java/com/planit/MainActivity.java
+++ PlanIt/src/main/java/com/planit/MainActivity.java
@@ -7,9 +7,12 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -33,6 +36,18 @@ public class MainActivity extends Activity {
 
         });
 
+    }
+
+    public void openCalendarEvent(View view){
+        Intent intent = new Intent(Intent.ACTION_INSERT);
+        intent.setType("vnd.android.cursor.item/event");
+        intent.putExtra(CalendarContract.Events.TITLE, "Learn to add events");
+        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "BCB lebbsss");
+        intent.putExtra(CalendarContract.Events.DESCRIPTION, "This may be pretty tough to do though....");
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, new Date());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+        intent.setData(CalendarContract.Events.CONTENT_URI);
+        startActivity(intent);
     }
 
 }
