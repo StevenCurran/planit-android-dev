875f653e6939491c04b2df71d4907818597cdeb4
StevenCurran <scurran14@qub.ac.uk>
2014-02-22 16:29:03 +0000

    pull in the loaded javascript which is the object that will be used for the user login

--- PlanIt/src/main/java/com/planit/WebViewActivity.java
+++ PlanIt/src/main/java/com/planit/WebViewActivity.java
@@ -4,14 +4,18 @@ package com.planit;
  * Created by Steven on 17/02/14.
  */
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpClient;
 import com.planit.constants.GlobalCookieStore;
 import com.planit.constants.UrlServerConstants;
 
@@ -19,7 +23,15 @@ import org.apache.http.impl.cookie.BasicClientCookie;
 
 public class WebViewActivity extends Activity {
 
+    final ValueCallback<String> valueCallback = new ValueCallback<String>() {
+        @Override
+        public void onReceiveValue(String s) {
+            Log.d("JS RESPONSE", s);
+
+        }
+    };
     private WebView webView;
+    private AsyncHttpClient client = new AsyncHttpClient();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,31 +40,37 @@ public class WebViewActivity extends Activity {
 
         webView = (WebView) findViewById(R.id.webView1);
         webView.getSettings().setJavaScriptEnabled(true);
+
         webView.setWebViewClient(new WebViewClient() {
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap s) {
                 if ((url.contains(UrlServerConstants.GOOGLE_AUTH_REDIRECT) && !url.contains(UrlServerConstants.GOOGLE_HOME)) || url.contains(UrlServerConstants.FACEBOOK_AUTH_REDIRECT)) {
-
+                    //Should we enter here we know that the user has been validated.
+                    webView.setBackgroundColor(0x00000000);
+                    webView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+                    //this should make the webview transparent, which is kinda cool.
                     BasicClientCookie clientCookie = new BasicClientCookie("JSESSIONID", getCookie(UrlServerConstants.PLANIT_ROOT, "JSESSIONID"));
                     clientCookie.setVersion(1);
                     clientCookie.setDomain("planit-dev.herokuapp.com");
                     clientCookie.setPath("/");
                     GlobalCookieStore.getCookieStore().addCookie(clientCookie);
 
-
-                    try {
-                        int duration = Toast.LENGTH_SHORT;
-                        String login_type = getIntent().getExtras().getString("login_type");
-                        Toast toast = Toast.makeText(view.getContext(), login_type, duration);
-                        toast.show();
-                    } catch (Exception e) {
-                        //swallow
-                    }
-
-                    finish();
+                    client.setCookieStore(GlobalCookieStore.getCookieStore());
+                    //finish();
                     //This will stop the actvity so that the JSON result will not be displayed. This should probs change in case of error.....
                     //how ever we will still have acess to their cookie
+                }
+            }
+
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if ((url.contains(UrlServerConstants.GOOGLE_AUTH_REDIRECT) && !url.contains(UrlServerConstants.GOOGLE_HOME)) || url.contains(UrlServerConstants.FACEBOOK_AUTH_REDIRECT)) {
+                    super.onPageFinished(view, url);
+                    // this is where we should read in the result of what is pulled back.
+                    webView.evaluateJavascript("javascript:document.documentElement.innerText", valueCallback);
+                    finish();
                 }
             }
 
@@ -64,13 +82,13 @@ public class WebViewActivity extends Activity {
 
         });
 
-        if (getIntent().getExtras() != null){
+        if (getIntent().getExtras() != null) {
             if (getIntent().getExtras().getString("login_type").equals("google")) {
                 webView.loadUrl(UrlServerConstants.GOOGLE_LOGIN);
             } else if (getIntent().getExtras().getString("login_type").equals("facebook")) {
                 webView.loadUrl(UrlServerConstants.FACEBOOK_LOGIN);
             }
-        }else{
+        } else {
             webView.loadUrl(UrlServerConstants.PLANIT_ROOT);
         }
 
@@ -92,4 +110,11 @@ public class WebViewActivity extends Activity {
         return CookieValue;
     }
 
+    @Override
+    protected void onDestroy() {
+        if (this.isFinishing()) {
+            // This is called when the activity is finishing. So we need to redirect the correct place.
+        }
+        super.onDestroy();
+    }
 }
