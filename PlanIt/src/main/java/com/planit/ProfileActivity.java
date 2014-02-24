674f5a5dbc8ed1516f820bf8aac9bac45b724220
gsmithu <gsmith14@qub.ac.uk>
2014-02-24 14:39:08 +0000

    More Profile Screen

--- PlanIt/src/main/java/com/planit/ProfileActivity.java
+++ PlanIt/src/main/java/com/planit/ProfileActivity.java
@@ -7,6 +7,10 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Garf on 24/02/2014.
@@ -22,10 +26,20 @@ public class ProfileActivity extends Activity {
         setContentView(R.layout.profile);
 
         //set fonts
-        Typeface btnFont = Typeface.createFromAsset(getAssets(), "fonts/segoeuisl.ttf");
-        Button scheduleButtonText = (Button) findViewById(R.id.scheduleButton);
-        scheduleButtonText.setTypeface(btnFont);
+        Typeface uilFont = Typeface.createFromAsset(getAssets(), "fonts/segoeuisl.ttf");
+        Button scheduleButton = (Button) findViewById(R.id.scheduleButton);
+        scheduleButton.setTypeface(uilFont);
+        TextView title = (TextView) findViewById(R.id.screenTitle);
+        title.setTypeface(uilFont);
+        //set schedule button date
+        scheduleButton.setText(getDayString());
+    }
 
+    public String getDayString(){
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DATE);
+
+        return Integer.toString(day);
     }
 
     public void goToSchedule(View view){
