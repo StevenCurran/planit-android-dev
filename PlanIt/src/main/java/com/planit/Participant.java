c92fc7e425ef4a6fb6a367196f653e74262f1a4c
StevenCurran <scurran14@qub.ac.uk>
2014-03-18 02:56:53 +0000

    profile changes

--- PlanIt/src/main/java/com/planit/Participant.java
+++ PlanIt/src/main/java/com/planit/Participant.java
@@ -10,9 +10,27 @@ public class Participant extends User implements Parcelable{
 
     private Boolean attending;
 
+    public Participant(Parcel parcel) {
+        //fix this up
+    }
+
     public Boolean getAttending() { return attending; }
 
     public void setAttending(Boolean attending) { this.attending = attending; }
+
+
+    public static final Creator<Participant> CREATOR = new Creator<Participant>() {
+        @Override
+        public Participant createFromParcel(Parcel parcel) {
+            return new Participant(parcel);
+        }
+
+        @Override
+        public Participant[] newArray(int i) {
+            return new Participant[i];
+        }
+    };
+
 
     @Override
     public int describeContents() {
