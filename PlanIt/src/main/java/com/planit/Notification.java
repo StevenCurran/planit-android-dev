9baa8adc33ca95eedae8a6e63bd3f2e00eb40ec8
Gareth Smith <gas001@gmail.com>
2014-03-23 00:15:19 +0000

    Notifications screen

---
+++ PlanIt/src/main/java/com/planit/Notification.java
@@ -0,0 +1,26 @@
+package com.planit;
+
+/**
+ * Created by Gareth on 22/03/2014.
+ */
+public class Notification {
+
+    private String title;
+    private String details;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
